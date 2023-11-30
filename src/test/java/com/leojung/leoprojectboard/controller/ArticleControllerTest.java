b57e5095566ad97594d1971da7f6bdaaaffda3e0
outlawleojung <outlawleojung@gmail.com>
2023-11-30 22:55:25 +0900

    thymeleaf 설정 추가

---
+++ src/test/java/com/leojung/leoprojectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,73 @@
+package com.leojung.leoprojectboard.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc){
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
+
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+        ;
+    }
+    @Disabled("개발중")
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
+
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"))
+        ;
+    }
+    @Disabled("개발중")
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
+
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("article/search"))
+        ;
+    }
+
+    @Disabled("개발중")
+    @DisplayName("[view][GET] 게시글 해시태그 전용 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticlHashtagSearchView_thenReturnArticleHashtagSearchView() throws Exception {
+
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("article/search-hashtag"))
+        ;
+    }
+}
