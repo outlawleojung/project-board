bbaec40e16c9d9076bb383b2fe491836c9adafae
outlawleojung <outlawleojung@gmail.com>
2023-11-30 19:04:02 +0900

    querydsl 설정 추가

--- src/main/java/com/leojung/leoprojectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/leojung/leoprojectboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,31 @@
 package com.leojung.leoprojectboard.repository;
 
 import com.leojung.leoprojectboard.domain.ArticleComment;
+import com.leojung.leoprojectboard.domain.QArticle;
+import com.leojung.leoprojectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true);
+
+        bindings.including(root.content, root.createdAt, root.createdBy);
+
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    }
 }
