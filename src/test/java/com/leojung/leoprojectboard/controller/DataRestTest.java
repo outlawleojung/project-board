233dcf1e0088a3fa25741ff6a953ba7782bce3a3
outlawleojung <outlawleojung@gmail.com>
2023-11-30 17:41:33 +0900

    Spring Data Rest 는 불필요하므로 제외 처리 한다.

--- src/test/java/com/leojung/leoprojectboard/controller/DataRestTest.java
+++ src/test/java/com/leojung/leoprojectboard/controller/DataRestTest.java
@@ -1,10 +1,10 @@
 package com.leojung.leoprojectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@Disabled("String Data Rest 통합테스트는 불필요하므로 제외시킴")
 @DisplayName("Data Rest Test")
 @AutoConfigureMockMvc
 @Transactional
